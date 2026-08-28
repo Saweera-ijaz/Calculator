162b5b3d710406b5d61d8fd00e0d3f17a43e19ce
Saweera Ijaz <saweeraijaz494@gmail.com>
2026-08-28 10:59:26 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -143,9 +143,5 @@ namespace MYCALCULATOR
             textBox1.Clear();
         }
 
-        private void saweeraIjaz5894_Load(object sender, EventArgs e)
-        {
-
-        }
-    }
+     }
 }
